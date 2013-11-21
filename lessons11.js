00df337e007585c99e5b3cc6a16470564f53dbae
Sasha <dimurchik@ukr.net>
2013-11-21 19:54:40 +0200

    начало строки цыркунфлекс(^) и мултилайн(m)

--- lessons11.js
+++ lessons11.js
@@ -2,14 +2,9 @@
  * Created by sasha on 21.11.13.
  */
 !function(){
-	console.log("".match(/.+/)); //null
-	console.log("".match(/./)); //null
-	console.log("asd".match(/.+/)); //asd
-	console.log("asd".match(/./)); //a
-	console.log("".match(/.*/)); //пуста стровка
-	console.log("asd".match(/.*/)); //asd
 
-	console.log("".match(/.?/)); //""
-	console.log("asd".match(/.?/)); //a
-
+	console.log("\nasdas\n44".match(/^\d/)); //null
+	console.log("\nasdas\n44".match(/^\d/m)); // 44
+	console.log("asdas44dd".match(/^\d/)); //null
+	console.log("1asdasdd".match(/^\d/)); //1
 }();
