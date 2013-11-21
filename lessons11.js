4ffcc2555789eb3376c36bb9ef438b9c65c591b1
Sasha <dimurchik@ukr.net>
2013-11-21 20:58:50 +0200

    как заменять с помощю регулярок

--- lessons11.js
+++ lessons11.js
@@ -3,7 +3,5 @@
  */
 !function(){
 
-	console.log("solid 23px #rad".match(/(?:\d+|asd)px #(\w+)/)); //"23px", "23"
-	console.log("solid 23px #rad".match(/(?:\d+|asd)px #([a-z])/)); //"23px", "23"
-
+	console.log("solid 23px #rad".replace(/\d+px #(\w+)/, '$1sergey')); 
 }();
